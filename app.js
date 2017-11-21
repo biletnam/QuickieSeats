9ae4934485ff5f52ad5ecd69cde4376c5b6abe18
Red Lips <migo_andres@yahoo.com>
2017-11-22 04:01:15 +0800

    +Mongoose Prelim +1st Register Page Done +Payment Methods init

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var browserSync = require('browser-sync');
 
+var routes = require('./client/routes/router.js')
+
 //global.jQuery = require('jquery');
 //var bootstrap = require('bootstrap');
 var app = express();
 
-app.use(express.static('client'));
+
 
 var bs = browserSync.create();
 
@@ -13,6 +15,9 @@ bs.init({
   proxy: "localhost:4000",
   files : ["client/**"]
 });
+
+app.use(express.static('client'));
+app.use('/', routes);
 
 app.use(function(req,res,next){
     res.status(404).end("404 not found");
