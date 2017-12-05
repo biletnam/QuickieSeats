4305ae5a6197d57655382f28e2ac45c175ce29e6
Red Lips <migo_andres@yahoo.com>
2017-12-05 13:29:45 +0800

    ???????????Can run without error? Can insert data through mongodb CLI

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 // var browserSync = require('browser-sync');
-
 var bodyParser = require('body-parser');
-
 var routes = require('./client/routes/router.js')
-
 var path = require('path');
-
+var session = require('express-session');
 var mongoose  = require('mongoose');
-
+var MongoStore = require('connect-mongo')(session);
 
 //global.jQuery = require('jquery');
 //var bootstrap = require('bootstrap');
@@ -40,26 +37,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-//authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
 
 app.use(express.static('client'));
 app.use('/', routes);
