123ee21e163de75d4c494d2c854df02770d8ce1b
Red Lips <migo_andres@yahoo.com>
2017-12-16 15:31:43 +0800

    Navbar is decent. I still hate the syntax tho

--- public/js/script.js
+++ public/js/script.js
@@ -11,19 +11,45 @@ class AppBox extends React.Component {
 
     render() {
         const textRight = {
-          pullRight: 'true'
+          float: 'right'
         };
 
         return (
         <Router>
             <div>
               <header>
-                <nav className="navbar navbar-light justify-content-start">
+                {/* <nav className="navbar navbar-light jutify-content-start">
                   <a href="index.html" className="navbar-brand" >QuickieSeats</a>
-                  <a href="movies.html" className="navbar-brand">Movies</a>
-                  <a href="#" className="navbar-brand">Cinemas</a>
-                  <a href="#" className="navbar-brand">About</a>
-                  <a href="#" style={textRight} className="navbar-brand text-right" data-toggle="modal" data-target="#loginModal">Login/Register</a>
+                  <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <a href="movies.html" className="nav-link">Movies</a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="#" className="nav-link">Cinemas</a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="#" className="nav-link">About</a>
+                      </li>
+                    </ul>
+                    <div className="my-2 my-lg-0">
+                      <a href="#" className="nav-link" data-toggle="modal" data-target="#loginModal">Login/Register</a>
+                    </div>
+                  </div>
+                </nav> */}
+                <nav className="navbar navbar-light navbar-expand-lg">
+                  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <a className="navbar-brand" href="index.html">QuickieSeats</a>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                      <li><a href="movies.html" className="navbar-brand">Movies</a></li>
+                      <li><a href="#" className="navbar-brand">Cinemas</a></li>
+                      <li><a href="#" className="navbar-brand">About</a></li>
+                      <li><a href="#" className="navbar-brand" data-toggle="modal" data-target="#loginModal">Login/Register</a></li>
+                    </ul>
+                  </div>
                 </nav>
               </header>
 
