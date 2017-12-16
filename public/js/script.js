ffc1a0a5b4197539d37236388b5b392f4d85ba90
Red Lips <migo_andres@yahoo.com>
2017-12-16 14:31:45 +0800

    I'm starting to hate this syntax more than I hate myself -Login/REgister needs to PULL RIGHT FFS

--- public/js/script.js
+++ public/js/script.js
@@ -6,28 +6,35 @@ const Switch = window.ReactRouterDOM.Switch;
 const Redirect = window.ReactRouterDOM.Redirect;
 const hashHistory = window.ReactRouterDOM.hashHistory;
 
+
 class AppBox extends React.Component {
 
     render() {
+        const textRight = {
+          pullRight: 'true'
+        };
+
         return (
         <Router>
             <div>
-            <nav className="navbar navbar-light bg-light justify-content-between">
-                <a className="navbar-brand">Scrumie</a>
-            </nav>
-            <div id="content">
-                {/* <Route exact path="/meeting/new" component={MeetingNewBox} />
-                <Route exact path="/session/new" component={LoginBox} />
-                <Route exact path="/meetings/:meetingId" component={MeetingEditBox} />
-                <Route exact path="/" component={ScrumBox} /> */}
-                <h1>Kill me</h1>
+              <header>
+                <nav className="navbar navbar-light justify-content-start">
+                  <a href="index.html" className="navbar-brand" >QuickieSeats</a>
+                  <a href="movies.html" className="navbar-brand">Movies</a>
+                  <a href="#" className="navbar-brand">Cinemas</a>
+                  <a href="#" className="navbar-brand">About</a>
+                  <a href="#" style={textRight} className="navbar-brand text-right" data-toggle="modal" data-target="#loginModal">Login/Register</a>
+                </nav>
+              </header>
+
+              <div className="container" id="content">
+              </div>
+
+              <footer>
+                <span className="footer-text">(c) QuickieSeats 2017.</span>
+              </footer>
             </div>
-                    <hr />
-        <footer className="footer">
-            <p>(c) Scrumie 2017</p>
-        </footer>
-            </div>
-      </Router>
+        </Router>
 
         );
     }
