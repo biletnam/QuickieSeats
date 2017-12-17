663795221efaa65add38e679b84e060e7b2e042d
Red Lips <migo_andres@yahoo.com>
2017-12-17 18:48:18 +0800

    register account works

--- public/js/register-component.js
+++ public/js/register-component.js
@@ -1,21 +1,77 @@
 class RegisterBox extends React.Component {
 
-    constructor() {
-        super();
 
-        this.state ={
-            message: ""
-        }
-    }
+      constructor() {
+          super();
 
-    render() {
-        return (
+          this.state = {
+              done: false
+          }
+      }
+
+      render() {
+          if(this.state.done) {
+              return (
+                  <Redirect to="/session/new" />
+              );
+          }
+
+          return (
           <div className="container-fluid">
-            <div className="row">
-              <h1>Register</h1>
-            </div>
-        </div>
-        );
-    }
+              <div className="row">
+                  <div className="col-sm"></div>
+                  <div className="col-sm" id="col-sm-user">
+                    <h1>Create Account</h1>
+                    <form onSubmit={this._handleSubmit.bind(this)}>
+                      <div className="form-group">
+                          <label htmlFor="name">Username</label>
+                          <input type="text" ref={(input) => this._name = input} className="form-control" id="name" placeholder="Username"/>
+                      </div>
+                      <div className="form-group">
+                          <label htmlFor="project">Password</label>
+                          <input type="password" ref={(input) => this._password = input} className="form-control" id="password" placeholder="Password"/>
+                      </div>
+                      <input type="submit" className="btn btn-primary" />
+                    </form>
+                  </div>
+                  <div className="col-sm"></div>
+              </div>
+          </div>
+          );
+      }
+
+      _handleClose(e) {
+          e.preventDefault(e);
+
+          this._close();
+      }
+
+      _handleSubmit(e) {
+          e.preventDefault();
+
+          let user = {
+              name: this._name.value,
+              password: this._password.value,
+          }
+
+          $.ajax({
+              type: "POST",
+              url: "/api/register",
+              data: user
+          }).done((user, status, xhr) => {
+              this._close();
+          }).fail((xhr) => {
+              console.log(xhr.status);
+          });
+
+
+      }
+
+      _close() {
+
+          this.setState({
+              done: true
+          });
+      }
 
 }
